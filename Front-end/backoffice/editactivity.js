79d1f258268ca1d8778ba09db2fcf506deda13a9
FranciscoForte16 <francisco.forte1605@gmail.com>
2021-05-19 20:24:25 +0100

    toto

---
+++ Front-end/backoffice/editactivity.js
@@ -0,0 +1,173 @@
+//depedencies
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var app = express();
+
+//middleware
+app.use(bodyParser.urlencoded);
+{
+    extended: false;
+}
+
+app.use(validator());
+
+app.post('');
+
+
+
+function editvalidation(){
+
+    var name_act = document.getElementById("name_Request").value;
+    var type_act = document.getElementById("type_Request").value;
+    var address_act = document.getElementById("address_Location").value;
+    var county_act = document.getElementById("county").value;
+    var district_act = document.getElementById("district").value;
+    var zip_act = document.getElementById("post_code").value;
+    var latitude_act = document.getElementById("latitude").value;
+    var longitude_act = document.getElementById("longitude").value;
+
+    var date_act = document.getElementById("date_Request").value;
+    var time_act = document.getElementById("time").value;
+
+    if(name_act == ''){
+        Swal.fire(
+            '',
+            'O nome tem de estar preenchido',
+            'error'
+        );
+        document.getElementById("name_Request").focus();
+        return false;
+    }
+
+
+    if(type_act == ''){
+        Swal.fire(
+            '',
+            'Por favor, indique o tipo de atividade',
+            'error'
+        );
+        document.getElementById("type_Request").focus();
+        return false;
+
+    }
+
+    if(photo_act == 0){
+        Swal.fire(
+            '',
+            'Por favor, insira uma foto da atividade',
+            'error'
+        );
+        document.getElementById("editarFotografia").focus();
+        return false;
+    }
+
+
+    if(date_act == ''){
+        Swal.fire(
+            '',
+            'A data da atividade tem de estar preenchida',
+            'error'
+            );
+            document.getElementById("date_Request").focus();
+            return false;
+    }
+
+
+    if(time_act == ''){
+        Swal.fire(
+            '',
+            'A hora da atividade tem de estar preenchida',
+            'error'
+            );
+            document.getElementById("time").focus();
+            return false;
+    }
+
+    if(address_act == ''){
+        Swal.fire(
+            '',
+            'Por favor, indique a respetiva morada da atividade',
+            'error'
+        );
+        document.getElementById("address_Location").focus();
+        return false;
+    }
+
+    if(county_act == ''){
+        Swal.fire(
+            '',
+            'Por favor, indique a respetiva localidade da atividade',
+            'error'
+        );
+        document.getElementById("county").focus();
+        return false;
+    }
+
+
+    if(district_act == ''){
+        Swal.fire(
+            '',
+            'Por favor, indique o respetivo distrito da atividade',
+            'error'
+        );
+        document.getElementById("district").focus();
+        return false;
+
+    } 
+
+
+    if(zip_act == ''){
+        Swal.fire(
+            '',
+            'Por favor, indique o respetivo email para registo',
+            'error'
+        );
+        document.getElementById("post_code").focus();
+        return false;
+
+    } else if (zip_act.toString().length < 8 ){
+        Swal.fire(
+            '',
+            'O código postal deve conter 8 caracteres!',
+            'error'
+        );
+        document.getElementById("post_code").focus();
+        return false;
+
+    } else if (zip_act.toString().length > 8 ){
+        Swal.fire(
+            '',
+            'O código postal deve conter 8 caracteres!',
+            'error'
+        );
+        document.getElementById("post_code").focus();
+        return false;
+    }
+
+
+    if(latitude_act == ''){
+        Swal.fire(
+            '',
+            'Por favor, indique uma password para registo',
+            'error'
+        );
+        document.getElementById("latitude").focus();
+        return false;
+    }
+
+    if(longitude_act == ''){
+        Swal.fire(
+            '',
+            'Por favor, insira novamente a nova password',
+            'error'
+        );
+        document.getElementById("longitude").focus();
+        return false;
+
+    } 
+
+
+
+
+}
